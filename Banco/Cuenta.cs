e449d7047590243662fb1690c16a0f6ee0ac191b
Ingrid Luna <ingrid_lu1304@hotmail.com>
2018-10-10 12:25:00 -0500

    Completar

--- Banco/Cuenta.cs
+++ Banco/Cuenta.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Banco
+﻿namespace Banco
 {
-    class Cuenta
+    public class Cuenta
     {
+        public Cuenta(int id)
+        {
+            Id = id;
+            Saldo = 0;
+        }
+
+        public int Id { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        public decimal Retirar(decimal retiro)
+        {
+            if (retiro > Saldo)
+            {
+                throw new System.ArgumentException("Saldo insuficiente");
+            }
+            else
+            {
+                return Saldo -= retiro;
+            }
+        }
+
+        public decimal Abonar(decimal abono)
+        {
+            return Saldo += abono;
+        }
     }
 }
