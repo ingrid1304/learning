8026325c22c4800df9cc065560319bc81422b2d2
Ingrid Luna <ingrid_lu1304@hotmail.com>
2018-10-12 14:17:36 -0500

    Agregar más pruebas

--- Pruebas/Banco.cs
+++ Pruebas/Banco.cs
@@ -2,49 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace Pruebas
+namespace Pruebas.Banco
 {
-    [TestClass]
-    public class BancoTest
-    {
-        [TestMethod]
-        public void SaldoInicial()
-        {
-            Cuenta cuenta = new Cuenta(1);
-            Assert.AreEqual(cuenta.Saldo, 0);
-        }
-
-        [TestMethod]
-        public void AbonoCorrecto()
-        {
-            Cuenta cuenta = new Cuenta(1);
-            decimal resultado = cuenta.Abonar(500m);
-            Assert.AreEqual(cuenta.Saldo, 500m);
-            Assert.AreEqual(resultado, 500m);
-        }
-
-        [TestMethod]
-        public void RetiroCorrecto()
-        {
-            Cuenta cuenta = new Cuenta(1);
-            cuenta.Abonar(1000m);
-            decimal resultado = cuenta.Retirar(500m);
-            Assert.AreEqual(cuenta.Saldo, 500m);
-            Assert.AreEqual(resultado, 500m);
-        }
-
-        [TestMethod]
-        public void RetiroIncorrecto()
-        {
-            Cuenta cuenta = new Cuenta(1);
-            try
-            {
-                decimal resultado = cuenta.Retirar(500m);
-            }
-            catch (Exception e)
-            {
-                Assert.IsInstanceOfType(e, typeof(ArgumentException));
-            }
-        }
-    }
+    
 }
